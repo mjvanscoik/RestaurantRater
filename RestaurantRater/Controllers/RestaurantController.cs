d636a78e7dac60fc24170b42a123c35b7c1da8c5
Matthew VanScoik <vanscoik100@gmail.com>
2020-09-22 11:28:36 -0400

    Delete view and functionality added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,11 +10,56 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
-        private RestaurantDbContext _db = new RestaurantDBContext();
+        private RestaurantDbContext _db = new RestaurantDbContext();
         // GET: Restaurant
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //Brings you to a different place in the website.
+            }
+
+            return View(restaurant);
+        }
+        //GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //Post: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
